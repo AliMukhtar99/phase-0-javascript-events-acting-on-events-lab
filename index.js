5b74c6a35b96b08ff81082884c6b2013a1fcb978
AliMukhtar99 <ali.mukhta@student.moringaschool.com>
2022-08-09 20:40:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code here
+const moveDodgerLeft = () => {
+    const dodger = document.getElementById("dodger");
+    dodger.style.left = "50px";
+  };
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    dodger.style.left = "250px";
+  }
